e2fb25a9e9b93942e3e1f9424276beb4be727c40
DK0280705 <deka.ganteng028@gmail.com>
2023-07-19 06:35:14 +0700

    test: debug purposes

--- src/world/test/test_boat.gd
+++ src/world/test/test_boat.gd
@@ -8,7 +8,6 @@ func _process(_delta) -> void:
 	debug_string.emit(str("
 		V: ", boat.speed,"m/s,
 		V: ", boat.linear_velocity.length(), "px/s
-		a: ", (boat.thrust_force.length() - boat.get_drag_force())/boat.mass, "m/s^2
 		Linear damp: ", boat.get_linear_damp(), "
 		Drag Force: ", boat.get_drag_force(), "N,
-		Net Force: ", boat.thrust_force))
+		Thrust Force: ", boat.thrust_force))
