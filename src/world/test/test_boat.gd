25449b6fe2238426ae1c9dad13761499152b9d9e
DK0280705 <deka.ganteng028@gmail.com>
2023-07-19 05:21:48 +0700

    test: debug purposes

--- src/world/test/test_boat.gd
+++ src/world/test/test_boat.gd
@@ -3,8 +3,12 @@ extends Node2D
 @onready var boat : Base_ship = get_node("boat")
 signal debug_string(text: String)
 
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	#TODO: Get Net Force of boat.
-	debug_string.emit(str(
-		"V: ", floorf(boat.speed),"m/s,
-		Drag Force: ", floorf(boat.drag_force()), "N"))
+	debug_string.emit(str("
+		V: ", boat.speed,"m/s,
+		V: ", boat.linear_velocity.length(), "px/s
+		a: ", (boat.thrust_force.length() - boat.get_drag_force())/boat.mass, "m/s^2
+		Linear damp: ", boat.get_linear_damp(), "
+		Drag Force: ", boat.get_drag_force(), "N,
+		Net Force: ", boat.thrust_force))
