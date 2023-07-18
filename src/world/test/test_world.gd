d7dedd2ddec0d0e8ab7434806081ae0efcb10f13
DK0280705 <deka.ganteng028@gmail.com>
2023-07-19 05:22:05 +0700

    test: world viewport for pixel art

--- src/world/test/test_world.gd
+++ src/world/test/test_world.gd
@@ -4,12 +4,19 @@ extends Node2D
 
 @onready var test_node   : Node2D      = test_scene.instantiate()
 @onready var sub_viewport: SubViewport = $subviewport_container/subviewport
+@onready var sub_viewport_container: SubViewportContainer = $subviewport_container
 @onready var debug_label : Label       = $debug_ui/label
+
+func _set_svc_size() -> void:
+	#sub_viewport_container.scale = get_viewport_rect().size
+	pass
 
 func _ready() -> void:
 	assert(test_node.has_signal("debug_string"))
 	test_node.connect("debug_string", _on_debug_string)
 	sub_viewport.add_child(test_node)
+	_set_svc_size()
+	get_tree().root.connect("size_changed", _set_svc_size)
 
 func _on_debug_string(text: String) -> void:
 	debug_label.text = text
