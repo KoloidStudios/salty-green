ed2a3c56ad66bc16501daa95a850e3c975a25a77
DK0280705 <deka.ganteng028@gmail.com>
2023-07-19 05:20:40 +0700

    config: add inverse meter to pixel

--- src/autoloader/config.gd
+++ src/autoloader/config.gd
@@ -1,3 +1,8 @@
 extends Node
 
-var meter_to_pixel_multiplier = 10.0
+var meter_to_pixel_multiplier = 16.0
+var pixel_to_meter_multiplier = 1 / meter_to_pixel_multiplier
+
+func _ready():
+    DisplayServer.window_set_min_size(Vector2(640, 360))
+    print("Loaded config")
