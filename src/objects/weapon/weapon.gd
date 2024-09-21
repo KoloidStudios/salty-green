d7d27796630a7a904adf08f735ede0f33be593ab
RealPG125 <real125acc@gmail.com>
2024-09-22 02:00:39 +0900

    Add fire point

--- src/objects/weapon/weapon.gd
+++ src/objects/weapon/weapon.gd
@@ -3,13 +3,14 @@ class_name Weapon
 
 @export var _rotatable_node: Node2D
 @export var _projectile: PackedScene
+@export var _fire_point: Vector2
 
 func rotate_to(target: Vector2) -> void:
 	_rotatable_node.look_at(target)
 	
 func fire(target: Vector2) -> void:
 	var object: Weapon_projectile = _projectile.instantiate()
-	object.position = global_position
+	object.position = global_position + _fire_point.rotated(get_angle_to(target)) 
 	get_viewport().add_child(object)
-	object.activate(target)
+	object.activate(target)	
 	
